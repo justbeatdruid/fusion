cdd81dde490d9a18eb11a3b891f78ed7c63da6b9
wangmeijuan <542204218@qq.com>
2020-02-12 00:18:28 +0800

    流控策略绑定api，解绑api、删除接口，脚本合入

---
+++ scripts/trafficcontrol/bind.sh
@@ -0,0 +1,9 @@
+curl -XPOST localhost:8001/api/v1/trafficcontrols/247ef42b2f792bca/apis -H 'content-type:application/json'  -v \
+  -d'
+{
+  "data":[
+  { "id": "cb0213928864c594"
+   }
+  ]
+  
+}'
---
+++ scripts/trafficcontrol/unbind.sh
@@ -0,0 +1,9 @@
+curl -XDELETE localhost:8001/api/v1/trafficcontrols/247ef42b2f792bca/apis -H 'content-type:application/json'  -v \
+  -d'
+{
+  "data":[
+  { "id": "a40a646e35c9d79b"
+   }
+  ]
+  
+}'
