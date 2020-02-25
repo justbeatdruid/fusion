372691546bf051304b0de677b5ba3dddfaab550d
xuxu <xuxu@chinamobile.com>
2020-02-25 19:24:06 +0800

    update script

--- scripts/datasource/create.sh
+++ scripts/datasource/create.sh
@@ -2,6 +2,17 @@ curl localhost:8001/api/v1/datasources -H 'content-type:application/json' \
   -d'
 {
   "data": {
-    "name": "testdatasource"
+    "name": "testdatasource",
+    "type": "rdb",
+    "rdb": {
+      "type": "mysql",
+      "database": "mysql",
+      "connect": {
+        "host": "119.3.248.187",
+	"port": 3306,
+	"username": "root",
+	"password": "my-secret-pw"
+      }
+    }
   }
 }'
