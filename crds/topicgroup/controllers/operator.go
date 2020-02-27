086aa308ca38edfd4a44a2a7865be7ebe5e781d2
李夏婕 <lixiajie@lixiajiedeMacBook-Pro.local>
2020-02-27 12:24:59 +0800

    提交topicgroup代码

---
+++ crds/topicgroup/controllers/operator.go
@@ -0,0 +1,45 @@
+package controllers
+
+import (
+	"fmt"
+	v1 "github.com/chinamobile/nlpt/crds/topicgroup/api/v1"
+	"github.com/parnurzeal/gorequest"
+	"k8s.io/klog"
+)
+
+type Operator struct {
+	Host string
+	Port int
+}
+
+const namespaceUrl = "/admin/v2/namespaces/%s/%s"
+const protocol = "http"
+
+type requestLogger struct {
+	prefix string
+}
+
+var logger = &requestLogger{}
+
+func (r *requestLogger) SetPrefix(prefix string) {
+	r.prefix = prefix
+}
+
+func (r *requestLogger) Printf(format string, v ...interface{}) {
+	klog.V(4).Infof(format, v...)
+}
+
+func (r *requestLogger) Println(v ...interface{}) {
+	klog.V(4).Infof("%+v", v)
+}
+
+func (r *Operator) CreateNamespace(namespace *v1.Topicgroup) error {
+	request := gorequest.New().SetLogger(logger).SetDebug(true).SetCurlCommand(true)
+
+	url := fmt.Sprintf(namespaceUrl, namespace.Spec.Tenant, namespace.Spec.Name)
+	url = fmt.Sprintf("%s://%s:%d%s", protocol, r.Host, r.Port, url)
+
+	request.Put(url)
+	request.Send("").EndStruct("")
+	return nil
+}
