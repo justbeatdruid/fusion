1b9c3efcec207d774159aa2987649810d2b8f07e
xuxu <xuxu@chinamobile.com>
2020-03-09 11:05:59 +0800

    change patch req body for serviceunit

--- scripts/serviceunit/updatedata.sh
+++ scripts/serviceunit/updatedata.sh
@@ -6,14 +6,7 @@ fi
 curl -XPATCH localhost:8001/api/v1/serviceunits/$1 -H 'content-type:application/json' -H 'X-auth-Token:8c06d2a4d87a8e4df280589889d85f67e6d755b2fb33538fd724ba6f700361a0' \
   -d'
 {
-  "data,omitempty": {
-    "name": "sssssyyyy",
-    "type": "data",
-    "description": "simple serviceunit",
-    "datasources":
-      {
-        "id": "caeaa2f64e39cdcf"
-      },
-    "group": "182a3f334f835da7"
+  "data": {
+    "description": "simple serviceunit patch patch patch"
   }
 }'
