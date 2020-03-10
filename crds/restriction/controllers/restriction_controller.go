c9f74f9bd6ba95ac78e76fba6eee5261a3034f51
sunyu <761121443@qq.com>
2020-03-10 14:30:36 +0800

    restriction

--- crds/restriction/controllers/restriction_controller.go
+++ crds/restriction/controllers/restriction_controller.go
@@ -24,7 +24,6 @@ import (
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
-	"k8s.io/klog"
 	nlptv1 "github.com/chinamobile/nlpt/crds/restriction/api/v1"
 )
 
