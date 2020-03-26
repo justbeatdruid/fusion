760455ba6bd33b523fe916eb3ac0c1f65deabe85
sunyu <761121443@qq.com>
2020-03-26 22:54:02 +0800

    api_list接口添加判断流控防控是否绑定

--- pkg/util/options.go
+++ pkg/util/options.go
@@ -5,12 +5,16 @@ type Op struct {
 	user      string
 	namespace string
 	group     string
+	restriction string
+	trafficcontrol string
 }
 
 func (o Op) NameLike() string  { return o.nameLike }
 func (o Op) User() string      { return o.user }
 func (o Op) Group() string     { return o.group }
 func (o Op) Namespace() string { return o.namespace }
+func (o Op) Restriction() string { return o.restriction }
+func (o Op) Trafficcontrol() string { return o.trafficcontrol }
 
 type OpOption func(*Op)
 
@@ -18,6 +22,8 @@ func WithNameLike(s string) OpOption  { return func(op *Op) { op.nameLike = s } 
 func WithGroup(s string) OpOption     { return func(op *Op) { op.group = s } }
 func WithUser(s string) OpOption      { return func(op *Op) { op.user = s } }
 func WithNamespace(s string) OpOption { return func(op *Op) { op.namespace = s } }
+func WithRestriction(s string) OpOption { return func(op *Op) { op.restriction = s } }
+func WithTrafficcontrol(s string) OpOption { return func(op *Op) { op.trafficcontrol = s } }
 
 func OpList(opts ...OpOption) Op {
 	ret := Op{}
