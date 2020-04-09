3c3143f5d37b5f0abcebc9dbdb6b06577365ae66
wangmeijuan <542204218@qq.com>
2020-04-09 20:54:48 +0800

    流量控制支持多租户特性脚本修改

--- scripts/trafficcontrol/create.sh
+++ scripts/trafficcontrol/create.sh
@@ -1,5 +1,5 @@
 curl -i localhost:8001/api/v1/trafficcontrols -H 'content-type:application/json'  -H'X-auth-Token:3df7da2cea083eaab0cfbaaff9883a932cf7af92cea37f9b3b74ba9e5aee4fe8' \
-  -d'
+-H 'userId:6' -H'tenantId:3000' -d'
 {
   "data": {
     "name": "wmjtrafficupdate",
--- scripts/trafficcontrol/update.sh
+++ scripts/trafficcontrol/update.sh
@@ -6,7 +6,7 @@ fi
 
 
 curl -i -XPATCH localhost:8001/api/v1/trafficcontrols/$1 -H 'content-type:application/json'  -H'X-auth-Token:3df7da2cea083eaab0cfbaaff9883a932cf7af92cea37f9b3b74ba9e5aee4fe8' \
-  -d'
+-H 'userId:6' -H'tenantId:3000'  -d'
 {
   "data": {
     "name": "trafficcontroltestupdate",
