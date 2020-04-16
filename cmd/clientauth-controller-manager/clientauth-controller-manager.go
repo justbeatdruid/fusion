b2ae855e123f0e3673356d6aac44540f8d96aca5
DGB <DGB@example.com>
2020-04-16 09:59:23 +0800

    add clientauth klog

--- cmd/clientauth-controller-manager/clientauth-controller-manager.go
+++ cmd/clientauth-controller-manager/clientauth-controller-manager.go
@@ -26,6 +26,7 @@ import (
 	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+	"k8s.io/klog"
 	// +kubebuilder:scaffold:imports
 )
 
@@ -39,6 +40,7 @@ func init() {
 
 	_ = nlptv1.AddToScheme(scheme)
 	// +kubebuilder:scaffold:scheme
+	klog.InitFlags(nil)
 }
 
 func main() {
