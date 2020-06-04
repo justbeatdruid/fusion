4bc1a5e8b4e4a0d0df3dcdcb5d25aedd9f9783a8
zhangru <zhangru@chinamobile.com>
2020-06-04 10:19:49 +0800

    dataservice

--- apiserver/database/model/dataintegration.go
+++ apiserver/database/model/dataintegration.go
@@ -5,6 +5,7 @@ import (
 	"time"
 
 	"github.com/astaxie/beego/orm"
+	"k8s.io/klog"
 )
 
 //Task ...
@@ -17,6 +18,11 @@ type Task struct {
 	SchedualPlan     string `orm:"size(5000)"`
 	DataSourceConfig string `orm:"size(5000)"`
 	DataTargetConfig string `orm:"size(5000)"`
+	Namespace        string
+	UserId           string
+	Status           bool
+	Job              string
+	StartTime        time.Time
 	CreatedTime      time.Time
 }
 
@@ -30,20 +36,38 @@ type TbDagRun struct {
 	Remark    string `orm:"size(5000)"`
 }
 
+//TbMetadata ...
+type TbMetadata struct {
+	DagId           string `orm:"pk;unique"`
+	DbId            string
+	Owner           string
+	TableName       string `orm:"size(128)"`
+	ColumnName      string `orm:"size(128)"`
+	OrdinalPosition int
+	ColumnType      string
+	Charset         string
+	Comments        string
+	IsAdd           int
+	AddValue        string
+	StatusFlag      int
+	PartitionInfo   string
+	Remark          string
+	CreateTime      time.Time
+	UpdataTime      time.Time
+}
+
+func AddTbMetadata(md *TbMetadata) error {
+	o := orm.NewOrm()
+	_, err := o.Insert(md)
+	return err
+
+}
+
 // AddTask ...
 func AddTask(m *Task) (id int64, err error) {
 	o := orm.NewOrm()
-	v := Task{DagId: m.DagId}
+	id, err = o.Insert(m)
 
-	err = o.QueryTable("Task").Filter("DagId", m.DagId).One(&v)
-	if err == nil {
-		v.Name = m.Name
-		_, err = o.Update(&v)
-		id = int64(v.Id)
-	} else {
-		id, err = o.Insert(m)
-
-	}
 	return
 }
 
@@ -58,46 +82,49 @@ func GetTaskByID(id int) (v *Task, err error) {
 }
 
 // GetTaskByDagId ...
-func GetTaskByDagId(dagId string) (v *Task, err error) {
+func GetTaskByDagId(dagId, userId, namespace string) (v *Task, err error) {
 	o := orm.NewOrm()
 
 	v = &Task{DagId: dagId}
-	err = o.QueryTable("Task").Filter("DagId", dagId).One(v)
+	err = o.QueryTable("Task").Filter("DagId", dagId).Filter("UserId", userId).Filter("Namespace", namespace).One(v)
 	return v, err
 }
 
 // GetTaskByName ...
-func GetTaskByName(name string) (v *Task, err error) {
+func GetTaskByName(name, userId, namespace string) (v *Task, err error) {
 	o := orm.NewOrm()
 
-	v = &Task{Name: name}
-	err = o.QueryTable("Task").Filter("Name", name).One(v)
+	v = &Task{
+		Name:      name,
+		UserId:    userId,
+		Namespace: namespace,
+	}
+	err = o.QueryTable("Task").Filter("Name", name).Filter("UserId", userId).Filter("Namespace", namespace).One(v)
 	return v, err
 }
 
 // DeleteTaskByDagId ...
-func DeleteTaskByDagId(dagId string) (v *Task, err error) {
+func DeleteTaskByDagId(dagId, userId, namespace string) (v *Task, err error) {
 	o := orm.NewOrm()
 	v = &Task{DagId: dagId}
-	err = o.QueryTable("Task").Filter("DagId", dagId).One(v)
+	err = o.QueryTable("Task").Filter("DagId", dagId).Filter("UserId", userId).Filter("Namespace", namespace).One(v)
 	if err != nil {
 		return
 	}
-	_, err = o.QueryTable("Task").Filter("DagId", dagId).Delete()
+	_, err = o.QueryTable("Task").Filter("DagId", dagId).Filter("UserId", userId).Filter("Namespace", namespace).Delete()
 	return
 }
 
 // UpdateTaskByID ...
-func UpdateTaskByID(m *Task) (err error) {
+func UpdateTaskByID(m *Task) error {
 	o := orm.NewOrm()
-	v := Task{Id: m.Id}
-	if err = o.Read(&v); err == nil {
-		var num int64
-		if num, err = o.Update(m); err == nil {
-			fmt.Println("Number of records updated in database:", num)
-		}
+
+	num, err := o.Update(m)
+	if err == nil {
+		klog.Errorf("Number of records updated in database:%v", num)
+
 	}
-	return
+	return err
 }
 
 // DeleteTaskByID ...
@@ -113,8 +140,88 @@ func DeleteTaskByID(id int) (err error) {
 }
 
 // GetTasks ...
-func GetTasks(offet, limit int) (tasks []Task, err error) {
+func GetTasks(offet, limit int, name, userId, namespace string) (tasks []Task, num, total int64, err error) {
 	o := orm.NewOrm()
-	_, err = o.QueryTable("Task").Offset(offet).Limit(limit).OrderBy("-Id").All(&tasks)
+	num, err = o.QueryTable("Task").Filter("Name__contains", name).Filter("UserId", userId).Filter("Namespace", namespace).Offset(offet).Limit(limit).OrderBy("-Id").All(&tasks)
+
+	total, err = o.QueryTable("Task").Filter("Name__contains", name).Filter("UserId", userId).Filter("Namespace", namespace).Count()
 	return
+}
+
+// GetTbDagRun ...
+func GetTbDagRun(dagID string) (dagRun []TbDagRun, num int64, err error) {
+	o := orm.NewOrm()
+	num, err = o.QueryTable("TbDagRun").Filter("DagId", dagID).OrderBy("-ExecDate").All(&dagRun)
+	return
+}
+
+// OperationTaskStatus ...
+func OperationTaskStatus(operation, userId, namespace string, ids []string) (task []Task, err error) {
+	o := orm.NewOrm()
+	_, err = o.QueryTable("Task").Filter("DagId__in", ids).Filter("UserId", userId).Filter("Namespace", namespace).All(&task)
+	if err != nil {
+		return task, err
+	}
+
+	if operation == "delete" {
+		p, err := o.Raw("DELETE  FROM task  WHERE dag_id = ? and user_id = ? and namespace = ?").Prepare()
+
+		for i := range ids {
+			_, err = p.Exec(ids[i], userId, namespace)
+
+			if err != nil {
+				klog.Errorf("exec sql failed,err: %v", err)
+			}
+
+		}
+		p.Close()
+		return task, err
+	}
+
+	p, err := o.Raw("UPDATE task SET status = ? WHERE dag_id = ? and user_id = ? and namespace = ?").Prepare()
+
+	for i := range ids {
+		if operation == "stop" {
+			_, err = p.Exec(false, ids[i], userId, namespace)
+
+		} else {
+			_, err = p.Exec(true, ids[i], userId, namespace)
+
+		}
+		if err != nil {
+			klog.Errorf("exec sql failed,err: %v", err)
+		}
+
+	}
+	p.Close()
+	return task, err
+}
+
+// OperationTaskStatus ...
+func UpdateTaskJob(dagid, job string) error {
+
+	o := orm.NewOrm()
+	_, err := o.Raw("UPDATE task SET job = ? WHERE dag_id = ?", job, dagid).Exec()
+
+	return err
+}
+
+//GetTaskByStartTime ...
+func GetTaskByStartTime() (tasks []Task, err error) {
+	o := orm.NewOrm()
+	_, err = o.QueryTable("Task").Filter("StartTime__lte", time.Now()).Filter("Job", "").Limit(10).OrderBy("-StartTime").All(&tasks)
+
+	p, err := o.Raw("UPDATE task SET Job = ? WHERE dag_id = ? ").Prepare()
+	for i := range tasks {
+
+		_, err = p.Exec("default", tasks[i].DagId)
+		if err != nil {
+			klog.Errorf("exec sql failed,err: %v", err)
+		}
+
+	}
+	p.Close()
+
+	return
+
 }
--- apiserver/resources/dataservice/service/service.go
+++ apiserver/resources/dataservice/service/service.go
@@ -4,148 +4,177 @@ import (
 	"encoding/base64"
 	"encoding/json"
 	"fmt"
+	"strconv"
+	"time"
 
 	"github.com/chinamobile/nlpt/apiserver/database/model"
 	k8s "github.com/chinamobile/nlpt/apiserver/kubernetes"
+	dsv1 "github.com/chinamobile/nlpt/crds/datasource/api/v1"
+	"github.com/chinamobile/nlpt/pkg/names"
+	"github.com/chinamobile/nlpt/pkg/util"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/client-go/dynamic"
 	clientset "k8s.io/client-go/kubernetes"
 	"k8s.io/klog"
 )
 
 var crdNamespace = "default"
 
-// var oofsGVR = schema.GroupVersionResource{
-// 	Group:    v1.GroupVersion.Group,
-// 	Version:  v1.GroupVersion.Version,
-// 	Resource: "dataservices",
-// }
-
 //Service ...
 type Service struct {
-	//client     dynamic.NamespaceableResourceInterface
+	dsClient   dynamic.NamespaceableResourceInterface
 	kubeClient *clientset.Clientset
 }
 
 //NewService ...
-func NewService(kubeClient *clientset.Clientset) *Service {
-	return &Service{
-		// client:     client.Resource(oofsGVR),
+func NewService(kubeClient *clientset.Clientset, client dynamic.Interface) *Service {
+	service := &Service{
+		dsClient:   client.Resource(dsv1.GetOOFSGVR()),
 		kubeClient: kubeClient,
 	}
+
+	//go service.dealIntegrationTask()
+
+	return service
 }
 
-// func NewService(client dynamic.Interface, kubeClient *clientset.Clientset) *Service {
-// 	return &Service{
-// 		client:     client.Resource(oofsGVR),
-// 		kubeClient: kubeClient,
-// 	}
-// }
+//CreateDataservice ...
+func (s *Service) CreateDataservice(dataService *Dataservice, userId, namespace string) (*Dataservice, error) {
+	_, err := model.GetTaskByName(dataService.Name, userId, namespace)
+	if err == nil {
+		return dataService, fmt.Errorf("name duplicated")
+	}
+	err = dataService.Validate(s, util.WithUser(userId), util.WithNamespace(namespace))
+	if err != nil {
+		return dataService, err
+	}
+
+	task := ToModel(dataService)
+	task.DagId = names.NewID()
+	dataService.DagID = task.DagId
+	task.Namespace = namespace
+	task.UserId = userId
+	task.Status = false
+	dataService.CreatedAt = task.CreatedTime.Format(TimeStr)
+	id, err := model.AddTask(task)
+	if err != nil {
+		return nil, err
+
+	}
+	dataService.ID = int(id)
+
+	// if err == nil {
+	// 	errJob := s.CreateJob(dataService.DagID, dataService.Name+"-"+dataService.DagID, dataService.SchedualPlanConfig.QuartzCronExpression, "registry.cmcc.com/library/smallcurl:1.0")
+	// 	klog.Errorf("errJob:%v", errJob)
+	// }
+
+	// klog.Errorf("DeleteCronJob errJob:%v", err)
+
+	return dataService, err
+}
 
 //CreateDataservice ...
-func (s *Service) CreateDataservice(dataService *Dataservice) (*Dataservice, error) {
-	_, err := model.GetTaskByName(dataService.Name)
-	if err == nil {
-		return dataService, fmt.Errorf("create error err: %+v,name: %v exist", err, dataService.Name)
-	}
-	task := ToModel(dataService)
-	dataService.DagID = task.DagId
-	id, err := model.AddTask(task)
-	dataService.ID = int(id)
-	if err == nil {
-		errJob := s.CreateJob(dataService.DagID, dataService.Name+"-"+dataService.DagID, dataService.SchedualPlanConfig.QuartzCronExpression, "registry.cmcc.com/library/smallcurl:1.0")
-		klog.Errorf("errJob:%v", errJob)
-	}
+func (s *Service) OperationDataservice(operationReq *OperationReq, userId, namespace string) error {
 
-	klog.Errorf("DeleteCronJob errJob:%v", err)
-
-	return ToAPI(task), err
+	if err := operationReq.Validate(); err != nil {
+		return err
+	}
+	_, err := model.OperationTaskStatus(operationReq.Operation, userId, namespace, operationReq.DagID)
+	return err
 }
 
 //ListDataservice ...
-func (s *Service) ListDataservice(offet, limit int) ([]*Dataservice, error) {
-	dss, err := model.GetTasks(offet, limit)
+func (s *Service) ListDataservice(offet, limit int, name, userId, namespace string) (interface{}, error) {
+	dss, num, total, err := model.GetTasks((offet-1)*limit, limit, name, userId, namespace)
 	if err != nil {
 		return nil, fmt.Errorf("cannot list object: %+v", err)
 	}
-	return ToListAPI(dss), nil
+	body := map[string]interface{}{}
+
+	body["items"] = ToListAPI(dss, s, util.WithUser(userId), util.WithNamespace(namespace))
+	body["count"] = num
+	body["total"] = total
+	body["page"] = offet
+	body["limit"] = limit
+
+	return body, nil
+}
+func (s *Service) GetDataSource(id string, opts ...util.OpOption) (*dsv1.Datasource, string, error) {
+	crdNamespace := util.OpList(opts...).Namespace()
+	if len(crdNamespace) == 0 {
+		crdNamespace = "default"
+	}
+	crd, err := s.dsClient.Namespace(crdNamespace).Get(id, metav1.GetOptions{})
+	if err != nil {
+		return nil, "", fmt.Errorf("error get crd: %+v", err)
+	}
+	ds := &dsv1.Datasource{}
+	if err := runtime.DefaultUnstructuredConverter.FromUnstructured(crd.UnstructuredContent(), ds); err != nil {
+		return nil, "", fmt.Errorf("convert unstructured to crd error: %+v", err)
+	}
+	klog.V(5).Infof("get v1.datasource: %+v", ds)
+	return ds, ds.Spec.Name, nil
+
 }
 
 //GetDataservice ...
-func (s *Service) GetDataservice(id string) (*Dataservice, error) {
-	ds, err := model.GetTaskByDagId(id)
+func (s *Service) GetDataservice(id, userId, namespace string) (*Dataservice, error) {
+	ds, err := model.GetTaskByDagId(id, userId, namespace)
 	if err != nil {
 		return nil, fmt.Errorf("cannot get object: %+v", err)
 	}
-	return ToAPI(ds), nil
+
+	return ToAPI(ds, s, util.WithUser(userId), util.WithNamespace(namespace)), nil
 }
 
 //DeleteDataservice ...
-func (s *Service) DeleteDataservice(id string) error {
-	task, err := model.DeleteTaskByDagId(id)
+func (s *Service) DeleteDataservice(id, userId, namespace string) error {
+	task, err := model.DeleteTaskByDagId(id, userId, namespace)
 	if err == nil {
-		errCronjob := k8s.DeleteCronJob(s.kubeClient, task.Name+"-"+task.DagId, crdNamespace)
+		errCronjob := k8s.DeleteCronJob(s.kubeClient, task.Job, crdNamespace)
 		klog.Errorf("errJob:%v", errCronjob)
 	}
 	return err
 }
 
-// func (s *Service) Create(ds *v1.Dataservice) (*v1.Dataservice, error) {
-// 	klog.V(5).Infof("creating dataservice: %+v", *ds)
-// 	content, err := runtime.DefaultUnstructuredConverter.ToUnstructured(ds)
-// 	if err != nil {
-// 		return nil, fmt.Errorf("convert crd to unstructured error: %+v", err)
-// 	}
-// 	crd := &unstructured.Unstructured{}
-// 	crd.SetUnstructuredContent(content)
+//UpdateDateService ...
+func (s *Service) UpdateDateService(reqData map[string]interface{}, dagId, userId, namespace string) (*Dataservice, error) {
+	taskdb, err := model.GetTaskByDagId(dagId, userId, namespace)
+	if err != nil {
+		klog.Errorf("get error:%v", err)
+		return nil, fmt.Errorf("not found")
+	}
+	if taskdb.Status {
+		klog.Errorf("task's status (%v) is running, please stop task first. :%v", taskdb.Status)
+		return nil, fmt.Errorf("task's status is running, please stop task first")
+	}
+	if err = s.assignment(taskdb, reqData); err != nil {
+		return nil, err
+	}
 
-// 	crd, err = s.client.Namespace(crdNamespace).Create(crd, metav1.CreateOptions{})
-// 	if err != nil {
-// 		return nil, fmt.Errorf("error creating crd: %+v", err)
-// 	}
+	if _, ok := reqData["Name"]; ok {
+		_, err = model.GetTaskByName(taskdb.Name, userId, namespace)
+		if err == nil {
+			return nil, fmt.Errorf("name duplicated")
+		}
+	}
+	api := ToAPI(taskdb, s, util.WithUser(userId), util.WithNamespace(namespace))
+	err = api.Validate(s, util.WithUser(userId), util.WithNamespace(namespace))
 
-// 	if err := runtime.DefaultUnstructuredConverter.FromUnstructured(crd.UnstructuredContent(), ds); err != nil {
-// 		return nil, fmt.Errorf("convert unstructured to crd error: %+v", err)
-// 	}
-// 	klog.V(5).Infof("get v1.dataservice of creating: %+v", ds)
-// 	return ds, nil
-// }
+	if err != nil {
+		return nil, err
+	}
 
-// func (s *Service) List() (*v1.DataserviceList, error) {
-// 	crd, err := s.client.Namespace(crdNamespace).List(metav1.ListOptions{})
-// 	if err != nil {
-// 		return nil, fmt.Errorf("error list crd: %+v", err)
-// 	}
-// 	dss := &v1.DataserviceList{}
-// 	if err := runtime.DefaultUnstructuredConverter.FromUnstructured(crd.UnstructuredContent(), dss); err != nil {
-// 		return nil, fmt.Errorf("convert unstructured to crd error: %+v", err)
-// 	}
-// 	klog.V(5).Infof("get v1.dataservicelist: %+v", dss)
-// 	return dss, nil
-// }
-
-// func (s *Service) Get(id string) (*v1.Dataservice, error) {
-// 	crd, err := s.client.Namespace(crdNamespace).Get(id, metav1.GetOptions{})
-// 	if err != nil {
-// 		return nil, fmt.Errorf("error get crd: %+v", err)
-// 	}
-// 	ds := &v1.Dataservice{}
-// 	if err := runtime.DefaultUnstructuredConverter.FromUnstructured(crd.UnstructuredContent(), ds); err != nil {
-// 		return nil, fmt.Errorf("convert unstructured to crd error: %+v", err)
-// 	}
-// 	klog.V(5).Infof("get v1.dataservice: %+v", ds)
-// 	return ds, nil
-// }
-
-// func (s *Service) Delete(id string) error {
-// 	err := s.client.Namespace(crdNamespace).Delete(id, &metav1.DeleteOptions{})
-// 	if err != nil {
-// 		return fmt.Errorf("error delete crd: %+v", err)
-// 	}
-// 	return nil
-// }
+	if err = model.UpdateTaskByID(taskdb); err != nil {
+		return nil, err
+	}
+	return api, err
+}
 
 //GetCmd ...
-func (s *Service) GetCmd(jobID string) ([]string, error) {
-	options, err := s.GetFlinkxBody(jobID)
+func (s *Service) GetCmd(task model.Task) ([]string, error) {
+	options, err := s.GetFlinkxBody(task)
 	if err != nil {
 		return []string{}, err
 	}
@@ -166,18 +195,73 @@ func (s *Service) GetCmd(jobID string) ([]string, error) {
 }
 
 //GetFlinkxBody ...
-func (s *Service) GetFlinkxBody(jobID string) (string, error) {
+func (s *Service) GetFlinkxBody(ds model.Task) (string, error) {
 	flinkJob := NewFlinkxReq()
+
+	// ds, err := model.GetTaskByDagId(jobID, userId, namespace)
+	// if err != nil {
+	// 	return "", fmt.Errorf("cannot get object: %+v", err)
+	// }
+	dataSource := DataSource{}
+	err := json.Unmarshal([]byte(ds.DataSourceConfig), &dataSource)
+	if err != nil {
+		klog.Errorf("unmarshal failed,err:%v, config:%v", err, ds.DataSourceConfig)
+		return "", err
+	}
+	sourceDB, _, err := s.GetDataSource(dataSource.RelationalDb.SourceID, util.WithUser(ds.UserId), util.WithNamespace(ds.Namespace))
+	if err != nil {
+		klog.Errorf("get source failed,err:%v", err)
+		return "", err
+	}
+
+	dataTarget := DataTarget{}
+	err = json.Unmarshal([]byte(ds.DataTargetConfig), &dataTarget)
+	if err != nil {
+		klog.Errorf("unmarshal failed,err:%v, config:%v", err, ds.DataTargetConfig)
+		return "", err
+	}
+	targetDB, _, err := s.GetDataSource(dataTarget.RelationalDbTarget.TargetID, util.WithUser(ds.UserId), util.WithNamespace(ds.Namespace))
+	if err != nil {
+		klog.Errorf("get source failed,err:%v", err)
+		return "", err
+	}
+
+	flinkJob.Job.Content[0].Reader.Parameter.UserName = sourceDB.Spec.RDB.Connect.Username
+	flinkJob.Job.Content[0].Reader.Parameter.Password = sourceDB.Spec.RDB.Connect.Password
+	flinkJob.Job.Content[0].Reader.Parameter.Connection[0].Table = []string{dataSource.RelationalDb.SourceTable}
+
+	flinkJob.Job.Content[0].Reader.Parameter.Connection[0].JdbcURL = []string{"jdbc:mysql://" + sourceDB.Spec.RDB.Connect.Host + ":" + strconv.Itoa(sourceDB.Spec.RDB.Connect.Port) + "/" + sourceDB.Spec.RDB.Database}
+	flinkJob.Job.Content[0].Writer.Parameter.UserName = targetDB.Spec.RDB.Connect.Username
+	flinkJob.Job.Content[0].Writer.Parameter.Password = targetDB.Spec.RDB.Connect.Password
+
+	flinkJob.Job.Content[0].Writer.Parameter.Connection[0].Table = []string{dataTarget.RelationalDbTarget.TargetTable}
+
+	flinkJob.Job.Content[0].Writer.Parameter.Connection[0].JdbcURL = []string{"jdbc:mysql://" + targetDB.Spec.RDB.Connect.Host + ":" + strconv.Itoa(targetDB.Spec.RDB.Connect.Port) + "/" + targetDB.Spec.RDB.Database}
+
+	for _, v := range dataTarget.RelationalDbTarget.MappingRelation {
+		flinkJob.Job.Content[0].Reader.Parameter.Column = append(flinkJob.Job.Content[0].Reader.Parameter.Column, v.SourceField)
+		flinkJob.Job.Content[0].Writer.Parameter.Column = append(flinkJob.Job.Content[0].Writer.Parameter.Column, v.SourceField)
+
+	}
+	klog.Infof("flinkJob:%v", flinkJob)
 	job, err := json.Marshal(&flinkJob)
 	if err != nil {
 		klog.Errorf("json Marshal failed,err:%v, flinkJob:%v", err, flinkJob)
 		return "", err
 	}
-	flinkxBody := map[string]interface{}{
-		"-jobid":      jobID,
+
+	config, err := json.Marshal(ds)
+	if err != nil {
+		klog.Errorf("json Marshal failed,err:%v, flinkJob:%v", err, flinkJob)
+		return "", err
+	}
+
+	flinkxBody := map[string]string{
+		"-jobid":      ds.DagId,
 		"-mode":       "local",
 		"-pluginRoot": "/root/flinkx-rest/plugins/",
 		"-job":        string(job),
+		"-config":     string(config),
 	}
 	klog.Infof("flinkxBody:%v", flinkxBody)
 	flinkxBytes, err := json.Marshal(flinkxBody)
@@ -189,16 +273,73 @@ func (s *Service) GetFlinkxBody(jobID string) (string, error) {
 }
 
 //CreateJob ...
-func (s *Service) CreateJob(jobID, name, schedule, imageName string) error {
-	cmd, err := s.GetCmd(jobID)
+func (s *Service) CreateJob(imageName string, task model.Task) error {
+	cmd, err := s.GetCmd(task)
 	if err != nil {
 		klog.Errorf("get cmd failed,err:%v", err)
 		return err
 	}
-	err = k8s.CreateCronJob(s.kubeClient, name, schedule, imageName, jobID, crdNamespace, cmd)
+	schedule, err := s.getSchedule(task)
+	if err != nil {
+		klog.Errorf("get schedual failed,err:%v", err)
+		return err
+	}
+	err = k8s.CreateCronJob(s.kubeClient, task.Name+"-"+task.DagId, schedule, imageName, task.DagId, crdNamespace, cmd)
 	if err != nil {
 		klog.Errorf("Create Job failed,err:%v", err)
 		return err
 	}
-	return nil
+	err = model.UpdateTaskJob(task.DagId, task.Name+"-"+task.DagId)
+	if err != nil {
+		klog.Errorf("update Job failed,err:%v", err)
+	}
+
+	return err
+}
+
+func (s *Service) getSchedule(task model.Task) (string, error) {
+	if task.Type == "realtime" {
+		return "*/1 * * * *", nil
+	}
+	config := SchedualPlan{}
+	if err := json.Unmarshal([]byte(task.SchedualPlan), &config); err != nil {
+		return "", err
+	}
+	if config.QuartzCron {
+		return config.QuartzCronExpression, nil
+	}
+	if config.TimeUnit == "minute" {
+		return "*/" + strconv.Itoa(config.SchedualPeriod) + " * * * *", nil
+	}
+	if config.TimeUnit == "hour" {
+		return "* */" + strconv.Itoa(config.SchedualPeriod) + " * * *", nil
+	}
+	if config.TimeUnit == "day" {
+		return "* * */" + strconv.Itoa(config.SchedualPeriod) + " * *", nil
+	}
+	if config.TimeUnit == "month" {
+		return "* * * */" + strconv.Itoa(config.SchedualPeriod) + " *", nil
+	}
+	return "* * * * */" + strconv.Itoa(config.SchedualPeriod), nil
+
+}
+
+func (s *Service) dealIntegrationTask() {
+	for {
+		time.Sleep(100 * time.Second)
+		task, err := model.GetTaskByStartTime()
+		if err != nil {
+			klog.Errorf("get task falied  Job failed,err:%v", err)
+			continue
+		}
+		for i := range task {
+			err = s.CreateJob("registry.cmcc.com/library/smallcurl:1.0", task[i])
+			if err != nil {
+				klog.Errorf("errJob:%v", err)
+				continue
+			}
+		}
+	}
+	return
+
 }
