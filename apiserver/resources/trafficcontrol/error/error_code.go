0c4a120c018c22799dcad22b124260bdb2aa4815
renleiguang <2546571794@qq.com>
2020-06-30 17:16:30 +0800

    流量控制跟新modifybug

--- apiserver/resources/trafficcontrol/error/error_code.go
+++ apiserver/resources/trafficcontrol/error/error_code.go
@@ -8,7 +8,7 @@ const(
 	MessageBodyIsEmpty										= "012000002"	//消息体为空
 	IncorrectAuthenticationInformation                      = "012000003"	//鉴权信息错误
 	FailedToCreateTrafficControl							= "012000004"	//创建流量控制失败
-	QuerySingleFlowControlFailureBasedOnId					= "012000005"	//根据id查询单个流量控制失败
+	QuerySingleFlowControlFailedById						= "012000005"	//根据id查询单个流量控制失败
 	FailedToDeleteFlowControl								= "012000006"	//删除流量控制失败
 	QueryFlowControlListFailed								= "012000007"	//查询流量控制列表失败
 	QueryFlowControlPagingParameterError					= "012000008"	//查询流量控制分页参数错误
@@ -16,7 +16,7 @@ const(
 	BindingOrUnbindingAPIFailed								= "012000010"	//绑定或者解绑api失败
 	FlowControlWithDuplicateName							= "012000011"	//流量控制名字重复
 	RequestParameterError									= "012000012"	//请求参数错误
-	crdFailedDuringCreation									= "012000013"	//crd创建时失败
+	CRDFailedDuringCreation									= "012000013"	//crd创建时失败
 	ErrorsInTheNumberOfAccessesPerUnitTimeLimit				= "012000014"	//每个单位时间限制的访问次数出错
 	ThereMustBeAtLeastOneTimeLimit							= "012000015"	//必须存在至少一个时间限制
 )
