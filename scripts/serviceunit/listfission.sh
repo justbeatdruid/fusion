f05295312047d5141c121790b87b1084374e86f7
sunyu <761121443@qq.com>
2020-07-17 17:12:15 +0800

    listFission.sh

---
+++ scripts/serviceunit/listfission.sh
@@ -0,0 +1,2 @@
+curl 10.160.32.24:30800/api/v1/serviceunits/fissions?$* -v \
+  -H'X-auth-Token:3df7da2cea083eaab0cfbaaff9883a932cf7af92cea37f9b3b74ba9e5aee4fe8' -H'userID:1' -H'tenantId:74a2512097335196b3040bed704c65c1'
