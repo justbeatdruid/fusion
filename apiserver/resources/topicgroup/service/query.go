34f40fe9d64d8bfe272405fcf2bb48a0fc3be481
李夏婕 <570056568@qq.com>
2020-07-24 10:52:35 +0800

    Topic分组性能优化

---
+++ apiserver/resources/topicgroup/service/query.go
@@ -0,0 +1,36 @@
+package service
+
+import (
+	"fmt"
+	v1 "github.com/chinamobile/nlpt/crds/topicgroup/api/v1"
+
+	"github.com/chinamobile/nlpt/apiserver/database/model"
+	"github.com/chinamobile/nlpt/pkg/util"
+
+	"k8s.io/klog"
+)
+
+func (s *Service) ListFromDatabase(opts ...util.OpOption) (*v1.TopicgroupList, error) {
+	op := util.OpList(opts...)
+	md := &model.TopicGroup{
+		Namespace: op.Namespace(),
+		Name:      op.NameLike(),
+		Available: op.Available(),
+	}
+	uid := op.User()
+
+	mapps, err := s.db.QueryTopicgroup(uid, md)
+	if err != nil {
+		return nil, fmt.Errorf("query topicgroup from database error: %+v", err)
+	}
+	tgs := []v1.Topicgroup{}
+	for _, mapp := range mapps {
+		tg, e := model.TopicgroupToApi(mapp)
+		if err != nil {
+			klog.Errorf("get serviceunit error: %+v", e)
+			continue
+		}
+		tgs = append(tgs, *tg)
+	}
+	return &v1.TopicgroupList{Items: tgs}, nil
+}
